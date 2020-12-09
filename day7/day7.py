755b96a175b258c1d754d482c66310465a4cd8ed
mbrylka <maciejbrylka94@gmail.com>
2020-12-09 09:51:33 +0100

    day seven part 1 complete

--- day7/day7.py
+++ day7/day7.py
@@ -1,30 +1,56 @@
-'''
-light red bags contain 1 bright white bag, 2 muted yellow bags.
-dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-bright white bags contain 1 shiny gold bag.
-muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-'''    
-
-'''
-Klasa?
-
-Lista nazw, jesli zawiera shiny gold to ją oznacz i pozniej szukaj po tej nazwie i usun ja z tablicy az do skutku
-'''
-
-
 with open('input.txt') as f:
     lines = f.readlines()
 lines = [x.strip() for x in lines]
 
+#clearing data
 bags = []
 for line in lines:
     line = line.replace(' bags', '')
     line = line.replace(' bag', '')
     line = line.replace(' contain', '')
+    line = line.replace(',', '')
+    line = line.replace('.', '')
     if 'no other' not in line:
         bags.append(line.split(' '))
 
-print(bags)
+#preparing data
+preparedBags = []
+for bag in bags:
+    name = bag[0] + ' ' + bag[1]
+    size = (len(bag)-2)//3
+    containing = ''
+    preparedBag = [name]
+    for i in range(size):
+        containing = bag[3+(i*3)]+' '+bag[4+(i*3)]
+        preparedBag.append(containing)
+    if(preparedBag[0] != 'shiny gold'):
+        preparedBags.append(preparedBag)
+    
+def intersection(lst1, lst2): 
+    lst3 = [value for value in lst1 if value in lst2] 
+    return len(lst3) > 0
+
+#searching
+#part 1
+counter = 0
+toSearch = ['shiny gold']
+newToSearch = []
+while len(toSearch)> 0:
+    for prepared in preparedBags:
+        if intersection(toSearch, prepared[1:]):
+            counter+=1
+            newToSearch.append(prepared[0])
+    toSearch = newToSearch
+    
+    newToSearch = []
+    preparedCopy = []
+    for prepared in preparedBags:
+        if prepared[0] not in toSearch:
+            preparedCopy.append(prepared)
+    
+    preparedBags = preparedCopy
+    print(toSearch)
+    
+print(counter)
+
+#part 2
