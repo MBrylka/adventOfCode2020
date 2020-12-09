5c48921ca6c844d8fc022aa86a91707029e165db
mbrylka <maciejbrylka94@gmail.com>
2020-12-09 18:20:44 +0100

    refactoring...

--- day7/day7.py
+++ day7/day7.py
@@ -25,80 +25,27 @@ class Bag:
     def addContent(self, name, quantity):
         self.contents.append([name, quantity])
 
-    def contains(self, name)
+    def contains(self, name):
         for con in self.contents:
             if con[0] == name:
                 return True
         return False
 
-    def getContents(self, name):
-        for con in self.contents:
-            if con[0] == name:
-                return con
+    def getContents(self):
+        return self.contents
 
-
-
+Bags = []
 #preparing data
-preparedBags = []
 for bag in bags:
     name = bag[0] + ' ' + bag[1]
+    b = None
+    b = Bag(name)
+
     size = (len(bag)-2)//3
     containing = ''
-    preparedBag = [name]
     for i in range(size):
         containing = bag[3+(i*3)]+' '+bag[4+(i*3)]
-        preparedBag.append(containing)
-        preparedBag.append(bag[2+(i*3)])
-    #if(preparedBag[0] != 'shiny gold'):
-        preparedBags.append(preparedBag)
-    
-def intersection(lst1, lst2): 
-    lst3 = [value for value in lst1 if value in lst2] 
-    return len(lst3) > 0
-
-#searching
-#part 1
-preparedBagsOriginal = preparedBags
-preparedBags2 = []
-for b in preparedBags:
-    if(b[0] != 'shiny gold'):
-        print(b)
-        preparedBags2. append(b)
-preparedBags = preparedBags2 #awful, needs refactoring
-counter = 0
-toSearch = ['shiny gold']
-newToSearch = []
-while len(toSearch) > 0:
-    for prepared in preparedBags:
-        if intersection(toSearch, prepared[1:]):
-            counter+=1
-            newToSearch.append(prepared[0])
-
-    toSearch = list(set(newToSearch))
-    
-    newToSearch = []
-    preparedCopy = []
-    for prepared in preparedBags:
-        if prepared[0] not in toSearch:
-            preparedCopy.append(prepared)
-    
-    preparedBags = preparedCopy
-    print(toSearch)
-
-print(counter)
-#part 2
-
-'''
-counter = 0
-toSearch = ['shiny gold']
-newToSearch = []
-preparedBags = preparedBagsOriginal
-while len(toSearch) > 0:
-    for prepared in preparedBags:
-        print(prepared[0])
-        if prepared[0] in toSearch:
-            print('found')
-            for x in range(1, (len(prepared)-1)/2):
-                print(prepared[x], prepared[x+1])
-        
-'''
+        quantity = bag[2+(i*3)]
+        b.addContent(containing, quantity)
+    Bags.append(b)
+    print(b)
