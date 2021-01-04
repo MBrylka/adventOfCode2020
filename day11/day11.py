7f40eb1d980190705362ab347b4d32bf4985bc16
mbrylka <maciejbrylka94@gmail.com>
2021-01-04 19:43:27 +0100

    day eleven complete

--- day11/day11.py
+++ day11/day11.py
@@ -16,7 +16,7 @@ for row in rows:
     map.append(list(a))
 
 map2 = deepcopy(map)
-
+mapOriginal = deepcopy(map)
 def printMap(m):
     for i in range(len(m)):
         for j in range(len(m[0])):
@@ -62,3 +62,115 @@ for i in range(len(map)):
             count+=1
 
 print(count)
+ 
+#part 2
+
+
+map = deepcopy(mapOriginal)
+map2 = deepcopy(map)
+while True:
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            counter = 0
+            if map[i][j] != '.':
+                #left
+                x = i
+                while x > 0:
+                    x-=1
+                    if map[x][j] == '#':
+                        counter += 1
+                        break
+                    elif map[x][j] =='L':
+                        break
+                #right
+                x = i
+                while x < len(map)-1:
+                    x+=1
+                    if map[x][j] == '#':
+                        counter += 1
+                        break
+                    elif map[x][j] =='L':
+                        break
+                #up
+                y = j
+                while y > 0:
+                    y-=1
+                    if map[i][y] == '#':
+                        counter += 1
+                        break
+                    elif map[i][y] == 'L':
+                        break
+                #down
+                y = j
+                while y < len(map[0])-1:
+                    y+=1
+                    if map[i][y] == '#':
+                        counter += 1
+                        break
+                    elif map[i][y] == 'L':
+                        break
+                #up-left
+                x = i
+                y = j
+                while x > 0 and y > 0:
+                    x-=1
+                    y-=1
+                    if map[x][y] == '#':
+                        counter += 1
+                        break
+                    elif map[x][y] == 'L':
+                        break
+                
+                #up-right
+                x = i
+                y = j
+                while x < len(map)-1 and y > 0:
+                    x+=1
+                    y-=1
+                    if map[x][y] == '#':
+                        counter += 1
+                        break
+                    elif map[x][y] == 'L':
+                        break
+                
+                #down-left
+                x = i
+                y = j
+                while x > 0 and y < len(map[0])-1:
+                    x-=1
+                    y+=1
+                    if map[x][y] == '#':
+                        counter += 1
+                        break
+                    elif map[x][y] == 'L':
+                        break
+                
+                #down-right
+                x = i
+                y = j
+                while x < len(map)-1 and y < len(map[0])-1:
+                    x+=1
+                    y+=1
+                    if map[x][y] == '#':
+                        counter += 1
+                        break
+                    elif map[x][y] == 'L':
+                        break
+                
+                if map[i][j] == 'L' and counter==0:
+                    map2[i][j] = '#'
+                elif map[i][j] == '#' and counter>=5:
+                    map2[i][j] = 'L'
+
+    if mapsTheSame(map, map2):
+        break
+    map = deepcopy(map2)
+
+count = 0
+for i in range(len(map)):
+    for j in range(len(map[0])):
+        if map[i][j] == '#':
+            count+=1
+
+print(count)         
+                   
